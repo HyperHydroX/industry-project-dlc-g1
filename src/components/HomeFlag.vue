71aa62bd08b5b4acd89523de8d0c5f299c595e12
HyperHydroX <guillaumedebackere7@gmail.com>
2022-06-09 12:02:05 +0200

    try change

--- src/components/HomeFlag.vue
+++ src/components/HomeFlag.vue
@@ -145,15 +145,15 @@ export default defineComponent({
   width: 100px;
   height: 2em;
   cursor: pointer;
-  // border-top: 1px solid #f9f9f9;
-  // border-left: 1px solid #f9f9f9;
-  // border-right: 1px solid #f9f9f9;
+  border-top: 0.1px solid #f9f9f9;
+  border-left: 0.1px solid #f9f9f9;
+  border-right: 0.1px solid #f9f9f9;
 }
 
-// .flag-part + .flag-part {
-//   border-top: none;
-//   border-bottom: 1px solid #f9f9f9;
-// }
+.flag-part + .flag-part {
+  border-top: none;
+  border-bottom: 0.1px solid #f9f9f9;
+}
 
 .flagPartLarge {
   width: 80%;
