5717989a4555c6ec976214ca5eeee67e47faf4f4
Umair Bhatti <umair.bhatti@student.howest.be>
2022-06-14 22:05:58 +0200

    change firebase file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-import { getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,10 +16,10 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-export const auth = getAuth();
+initializeApp(firebaseConfig)
+export const auth = getAuth()
 
-export const sponsers = [];
+export const sponsers = []
 import { user } from '../layouts/LoginLayout'
 
 // export const registerUser = (email, password) => {
@@ -33,36 +33,37 @@ import { user } from '../layouts/LoginLayout'
 //     console.log(error.message)
 //   });
 // }
-// export const loginUser = (email, password) => {
-//     signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // User credentials
-//     user = userCredential.user;
-//   })
-//     .catch((error) => {
-//       console.log(error.code)
-//       console.log(error.message)
-//   });
-// }
+export const loginUser = new Promise((resolve, reject, email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // User credentials
+      user = userCredential.user
+      resolve(user)
+    })
+    .catch((error) => {
+      console.log(error.code)
+      console.log(error.message)
+      reject(error)
+    })
+})
 export const uploadFile = async (file) => {
-    try {
-        console.log(file)
-        const storage = getStorage()
-        const storageRef = ref(storage, `/${user.uid}`+`/${file.name}`)
-            // 'file' comes from the Blob or File API
-        await uploadBytes(storageRef, file)
-            .then((snapshot) => {
-            getDownloadURL(ref(storage, snapshot.metadata.fullPath)).then((url) => {
-                sponsers.push(url)
-            })
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    } catch (error) {
-        console.log(file)
-        console.log(user)
-        console.log(error)
-    }
+  try {
+    console.log(file)
+    const storage = getStorage()
+    const storageRef = ref(storage, `/${user.uid}` + `/${file.name}`)
+    // 'file' comes from the Blob or File API
+    await uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        getDownloadURL(ref(storage, snapshot.metadata.fullPath)).then((url) => {
+          sponsers.push(url)
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  } catch (error) {
+    console.log(file)
+    console.log(user)
+    console.log(error)
+  }
 }
-
