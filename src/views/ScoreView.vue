d70e14e58e43cd057a4c88da0447ebae04387dc2
Umair Bhatti <umair.bhatti@student.howest.be>
2022-06-16 00:37:27 +0200

    timer flex

--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -90,22 +90,30 @@
           </div>
         </div>
       </div>
-              <!-- @click="setTime('toggle')" -->
+      <!-- @click="setTime('toggle')" -->
       <q-btn
-
         @click="updateTimerBord('toggle')"
         data-timer="toggle"
         class="q-btn timer-btn"
         label="rust"
       />
       <q-btn
-      @click="setTime('reset')"
+        @click="setTime('reset')"
         data-timer="reset"
         class="q-btn timer-btn"
         label="tijd resetten"
       />
       <div class="q-btn--small">
-        <svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M440.65 12.57l4 82.77A247.16 247.16 0 0 0 255.83 8C134.73 8 33.91 94.92 12.29 209.82A12 12 0 0 0 24.09 224h49.05a12 12 0 0 0 11.67-9.26 175.91 175.91 0 0 1 317-56.94l-101.46-4.86a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12H500a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12h-47.37a12 12 0 0 0-11.98 12.57zM255.83 432a175.61 175.61 0 0 1-146-77.8l101.8 4.87a12 12 0 0 0 12.57-12v-47.4a12 12 0 0 0-12-12H12a12 12 0 0 0-12 12V500a12 12 0 0 0 12 12h47.35a12 12 0 0 0 12-12.6l-4.15-82.57A247.17 247.17 0 0 0 255.83 504c121.11 0 221.93-86.92 243.55-201.82a12 12 0 0 0-11.8-14.18h-49.05a12 12 0 0 0-11.67 9.26A175.86 175.86 0 0 1 255.83 432z"/></svg>
+        <svg
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) -->
+          <path
+            d="M440.65 12.57l4 82.77A247.16 247.16 0 0 0 255.83 8C134.73 8 33.91 94.92 12.29 209.82A12 12 0 0 0 24.09 224h49.05a12 12 0 0 0 11.67-9.26 175.91 175.91 0 0 1 317-56.94l-101.46-4.86a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12H500a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12h-47.37a12 12 0 0 0-11.98 12.57zM255.83 432a175.61 175.61 0 0 1-146-77.8l101.8 4.87a12 12 0 0 0 12.57-12v-47.4a12 12 0 0 0-12-12H12a12 12 0 0 0-12 12V500a12 12 0 0 0 12 12h47.35a12 12 0 0 0 12-12.6l-4.15-82.57A247.17 247.17 0 0 0 255.83 504c121.11 0 221.93-86.92 243.55-201.82a12 12 0 0 0-11.8-14.18h-49.05a12 12 0 0 0-11.67 9.26A175.86 175.86 0 0 1 255.83 432z"
+          />
+        </svg>
       </div>
     </div>
   </div>
@@ -119,8 +127,8 @@ import { updateTeamScore } from '../firebase/firebase'
 // import { updateTimer } from '../firebase/firebase2'
 // import { updateTimerBord } from '../scoreboard/scoreboard'
 
-export let localMinites = document.querySelector(".js-thuis-score")
-export let localSeconds = document.querySelector(".js-thuis-score")
+export let localMinites = document.querySelector('.js-thuis-score')
+export let localSeconds = document.querySelector('.js-thuis-score')
 
 export default {
   name: 'ScoreView',
@@ -251,9 +259,8 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
 .container-timer {
-  display: flex
+  display: flex;
 }
 // Fonts
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
@@ -282,6 +289,16 @@ p {
   align-items: center;
   justify-content: space-between;
   overflow: hidden;
+}
+
+@media screen and (max-width: 380px) {
+  .q-kleuren {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    overflow: hidden;
+  }
 }
 
 .q-btn {
